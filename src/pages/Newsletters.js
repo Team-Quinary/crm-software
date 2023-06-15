1821cbd569f84768fe74654770e3d26b70b412b2
WaruniGunasena <121920439+WaruniGunasena@users.noreply.github.com>
2023-06-15 12:42:17 +0530

    after adding newsletter

--- src/pages/Newsletters.js
+++ src/pages/Newsletters.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Newsletter from '../components/newsletters/Newsletter'
+import Form from '../components/newsletters/Form'
 
 export default function Newsletters() {
   return (
     <div>
-      <Newsletter/>
+     <Form/>
     </div>
   )
 }
